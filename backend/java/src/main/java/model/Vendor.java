0b60fd689b31e2bd85de73599b1bbc3df17bbd55
MaheshRaturi-IE <mahesh.raturi@indianexpress.com>
2020-02-11 17:09:22 +0530

    vendor updated!

--- backend/java/src/main/java/model/Vendor.java
+++ backend/java/src/main/java/model/Vendor.java
@@ -1,5 +1,101 @@
 package model;
 
-public class Vendor {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="vendor")
+public class Vendor {
+    @Column
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String createdOn;
+    @Column
+    private String lastLogin;
+    @Column
+    private String apiKey;
+    @Column
+    private String apiSecret;
+    @Column
+    private int status;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(String lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
